0e9be9ed5e67adb1e1b75e69e88936adaefc901e
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-11-06 16:10:15 +0600

    test upd

--- send.php
+++ send.php
@@ -1,23 +1,16 @@
 
 <?php 
-var_dump($_POST);
+// var_dump($_POST);
 if(isset($_POST['submit'])){
     $to = "bnvstorm@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $subject = "Форма отправки сообщений с сайта";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-	
-    mail($to,$subject,$message,$headers);
-   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-    echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
-	echo "<br /><br /><a href='https://istanbul.kz'>Вернуться на сайт.</a>";
-
+$subject = "Письмо с сайта";
+$charset = "utf-8";
+$headerss ="Content-type: text/html; charset=$charset\r\n";
+$headerss.="MIME-Version: 1.0\r\n";
+$headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
+$msg = "Имя: ".$_POST["name"]."\n";$msg .= "Сообщение: ".$_POST["msg"]."\n";
+mail($to, $subject, $msg, $headerss);
+print "<script>alert(\"Сообщение успешно отправлено!\");window.location = window.location.href</script>";
 }
 
 ?>
