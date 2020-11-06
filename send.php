4f3b4fa6f8b91d0f929e7942ac858718cea1c7cb
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-11-06 15:44:11 +0600

    upd and bugfix

--- send.php
+++ send.php
@@ -6,15 +6,15 @@ if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $subject = "Форма отправки сообщений с сайта";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
 	
     mail($to,$subject,$message,$headers);
    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-    echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+    echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
 	echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
 
 }
@@ -23,5 +23,5 @@ if(isset($_POST['submit'])){
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
 function changeurl(){eval(self.location="https://istanbul.kz");}
-window.setTimeout("changeurl();",3000);
+window.setTimeout("changeurl();",2000);
 </script>
