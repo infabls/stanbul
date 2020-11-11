e66db8a0155dad2c0f984908c4f6aad3759faabf
infabls <infabls.com@gmail.com>
2020-11-11 14:12:19 +0600

    fix form controller

--- send.php
+++ send.php
@@ -2,18 +2,19 @@
 <?php 
 var_dump($_POST);
 if(isset($_POST['submit'])){
-    $to = "bnvstorm@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
+    $to = "istanbul.uka@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
+    $from = "mail@istanbul.kz"; // this is the sender's Email address
+    $name = $_POST['name']; 
     $subject = "Форма отправки сообщений с сайта";
     $subject2 = "Copy of your form submission";
-    $message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+    $message = $name . " c номером " . $_POST['email'] . " оставил сообщение:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
 	
-    mail($to,$subject,$message,$headers);
+    $test = mail($to,$subject,$message,$headers);
+    var_dump($test);
    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
     echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
 	echo "<br /><br /><a href='https://istanbul.kz'>Вернуться на сайт.</a>";
